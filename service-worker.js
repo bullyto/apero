d1983b5f5258f741f7fc2d9421e4b6e92b682baf
bullyto <apero.nuit.du.66@gmail.com>
2025-12-22 05:14:17 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,38 @@
+const CACHE_NAME = "aperos-pwa-v1-20251222a";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./service-worker.js",
+  "./assets/logo-header.png",
+  "./assets/og-image-1200x630.jpg",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png",
+  "./assets/apple-touch-icon.png",
+  "./assets/bottle1.png",
+  "./assets/bottle2.png",
+  "./assets/bottle3.png",
+  "./assets/bottle4.png",
+  "./assets/bottle5.png",
+  "./assets/bottle6.png",
+  "./assets/bottle7.png"
+];
 
-const CACHE='aperos-v1';
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./'])));
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
-self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin === self.location.origin) {
+    event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  }
 });
